35c5812880d66ca9ed41a9409ae7721dd3a0325e
sanyam <patilsanyam869@gmail.com>
2024-04-06 18:46:40 +0530

    added route and error middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express'
 import { config } from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import jobRouter from './routes/jobRoutes.js'
+import userRouter from './routes/userRoutes.js'
+import applicationRouter from './routes/applicationRoutes.js'
 
 const app = express()
 config({ path: './env' })
@@ -18,6 +21,10 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// dbConnection()
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/job', jobRouter)
+app.use('/api/v1/application', applicationRouter)
+
+app.use(errorMiddleware)
 
 export default app
