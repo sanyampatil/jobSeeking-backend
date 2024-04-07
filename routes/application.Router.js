39a03afda4cfe984228e02e4f485ef3cb5f1fe64
sanyam <patilsanyam869@gmail.com>
2024-04-07 10:10:14 +0530

    added jobApplication module

--- routes/application.Router.js
+++ routes/application.Router.js
@@ -1,4 +1,17 @@
-const { Router } = require('express')
+import express from 'express'
+import {
+  employerGetAllApplications,
+  jobseekerDeleteApplication,
+  jobseekerGetAllApplications,
+  postApplication
+} from '../controllers/applicationController.js'
+import { isAuthenticated } from '../middleware/auth.js'
 
-const router = Router()
+const router = express.Router()
+
+router.post('/post', isAuthenticated, postApplication)
+router.delete('/delete/:id', isAuthenticated, jobseekerDeleteApplication)
+router.get('/jobseeker/getall', isAuthenticated, jobseekerGetAllApplications)
+router.get('/employer/getall', isAuthenticated, employerGetAllApplications)
+
 export default router
